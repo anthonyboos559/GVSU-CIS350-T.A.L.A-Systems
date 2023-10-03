ce6010e89ecb98b98af4aef62c28cf8c83dc1433
Alec Mirambeau <alecmirambeau@gmail.com>
2023-10-03 09:44:29 -0400

    Added GUI class/file title

--- GUI.py
+++ GUI.py
@@ -3,6 +3,8 @@ import tkinter as tk
 class GUI:
     """Displays the GUI that will utilize the backend Logic"""
     root = tk.Tk()
+    root.title("Inventory Management - T.A.L.A")
+
     root.mainloop()
 
 
