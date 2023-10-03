200fe37ed12720eeefc9b87366700af1edb6a3f1
Alec Mirambeau <alecmirambeau@gmail.com>
2023-10-03 17:30:21 -0400

    Adding the database skeleton start

---
+++ src/Database.py
@@ -0,0 +1,26 @@
+import sqlite3 as sq
+class Database:
+
+    def __init__(self):
+        self.query = None
+
+    def edit_row(self):
+        pass
+
+    def add_row(self):
+        pass
+
+    def delete_row(self):
+        pass
+
+    # receive inputs from GUI and store the passed information in the instance variables and format the correct
+    # SQL command
+    def pass_to_database(self, query: str,):
+        pass
+
+    def format_sql_command(self, query: str):
+        pass
+
+
+
+
