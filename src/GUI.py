eec095b55a91027c937fad3b6d71ecef324b6a9f
Anthony Boos <tony@LilDeb.local>
2023-11-02 16:49:01 -0400

    Further refined structure

--- src/GUI.py
+++ src/GUI.py
@@ -9,39 +9,30 @@ class GUI:
         self.main.geometry("300x300")
         self.tables = {"Inventory":0, "Employee":1, "Member":2}
         self.table = tk.IntVar(self.main, 0)
-        self.buttons = {"Add":"+", "Delete":"-", "Alter":"a", "New":"n"}
-        self.selected_buttons = tk.StringVar(value=["Add", "Delete", "Alter", "New"])
-        table_frame = tk.Frame(master=self.main, width=100)
-        tk.Label(master= table_frame, text="Select table:").pack()
+        self.table_frame = tk.LabelFrame(master=self.main, text= "Table:")
+
+        tk.Label(master= self.table_frame, text="Select table:").pack()
         for text, value in self.tables.items():
-            tk.Radiobutton(master= table_frame, text= text, variable= self.table, value= value).pack(side=tk.TOP, ipady=5)
-        table_frame.place(x=0, y=0)
-        button_frame = tk.Frame(master=self.main)
-        tk.Listbox(master= button_frame, listvariable=self.selected_buttons, height=6).pack()
-        button_frame.place(x=100,y=0)
+            tk.Radiobutton(master= self.table_frame, text= text, variable= self.table, value= value).pack(side=tk.TOP, ipady=5)
+        self.table_frame.place(x=0, y=0)
+        
+        self.test = {"This":0, "Is":1, "A":2, "Test!":3}
+        
+        testdict = {"text":"test!"}
+        for child in self.table_frame.winfo_children():
+            child.configure(testdict)
+        
+        
+        self.field_frame = tk.Frame(master=self.main)
+
+        #tk.Listbox(master= self.field_frame, listvariable=self.selected_buttons, height=6).pack()
+        self.field_frame.place(x=100,y=0)
         txt = tk.Text(master= self.main, height = 8, width= 35)
         txt.place(x=10, y=125)
         txt.insert(tk.END, "ITEM | QUANTITY | PRICE\nApple | 245 | $2.67\nOrange | 34 | $1\nPizza | 1,000,000 | $0\nPierogi | 57 | $3.25")
         self.main.mainloop()
 
-    def clear_gui(self) -> None:
-        pass
 
-    def pass_to_database(self) -> None:
-        #Send components of the SQL query the Databse object
-        pass
-
-    def display_buttons(self) -> None:
-        pass
-
-    def get_results(self):
-        pass
-
-    def print_table(self):
-        pass
-
-    def get_input(self):
-        pass
 
 
 test = GUI()
